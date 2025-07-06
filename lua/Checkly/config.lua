4bfb040514308ee4a341aa07924a9aa45c9271df
Hecdin <Hecdinnicola@outlook.com>
2025-07-05 23:23:23 -0400

    wip: Fix errors

--- lua/Checkly/config.lua
+++ lua/Checkly/config.lua
@@ -2,10 +2,11 @@ local M = {}
 
 M.options = {
   bars = "█",
-  colwidth_counter = 14,
-  colwidth_name = 28,
+  colwidth_counter = 15,
+  colwidth_name = 30,
   colwidth_progress = 25,
   target_file = "Summary.md",
+  sub = {"^[%d]+[%:%.]%s", ""}
 }
 
 return M
